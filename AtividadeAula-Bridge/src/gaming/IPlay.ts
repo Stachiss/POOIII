0195dc4807a55f97de50aa70791cc4e35589fb10
Carlos Eduardo de Paula Stachissini <92668640+Stachiss@users.noreply.github.com>
2022-07-29 09:18:20 -0300

    Delete AtividadeAula-Bridge directory

--- AtividadeAula-Bridge/src/gaming/IPlay.ts
+++
@@ -1,4 +0,0 @@
-export default interface IPlay{
-    playing():void;
-    result():void;
-}
--- AtividadeAula-Bridge/src/platforms/IPlatform.ts
+++
@@ -1,4 +0,0 @@
-export default interface IPlatform {
-    configureGame():void;
-    authToken():void;
-}
